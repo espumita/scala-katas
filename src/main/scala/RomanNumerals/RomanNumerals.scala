b28b8755e7aacff0be2b0d1c4c240203c38c6570
Espumita <davidfalconnaranjo@gmail.com>
2019-03-14 21:25:16 +0000

    Refactor with collection

--- src/main/scala/RomanNumerals/RomanNumerals.scala
+++ src/main/scala/RomanNumerals/RomanNumerals.scala
@@ -3,7 +3,15 @@ package RomanNumerals
 case class RomanNumerals() {
 
   val romanNumbers = Map(
-    (100, "C")
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I")
   )
 
   def get(number: Int): String = {
@@ -12,17 +20,9 @@ case class RomanNumerals() {
     }
     if (number > 100) return "C"+ get(number - 100)
     if (number > 90) return "XC" + get(number - 90)
-    if (number == 90) return "XC"
     if (number > 50) return "L" + get(number - 50)
-    if (number == 50) return "L"
     if (number > 40) return "XL" + get(number - 40)
-    if (number == 40) return "XL"
     if (number > 10) return "X" + get(number - 10)
-    if (number == 10) return "X"
-    if (number == 9) return "IX"
-    if (number == 5) return "V"
-    if (number == 4) return "IV"
-    if (number == 1) return "I"
     get(number - 1) + "I"
   }
 
