8dbe5a256b4f722be3a8697257b6e44f4530ed41
Espumita <davidfalconnaranjo@gmail.com>
2019-03-18 18:19:33 +0000

    Green: a character starts full health and lvl 1

---
+++ src/main/scala/RPGCombat/GameCharacter.scala
@@ -0,0 +1,9 @@
+package RPGCombat
+
+case class GameCharacter() {
+
+  var health: Int = 1000
+
+  var level: Int = 1
+
+}
---
+++ src/test/scala/RPGCombat/CharacterShould.scala
@@ -0,0 +1,18 @@
+package RPGCombat
+
+import org.scalatest.{Matchers, WordSpec}
+
+class CharacterShould extends WordSpec with Matchers {
+
+
+
+
+  "a character starts full health and lvl 1"  in {
+    val aCharacter = GameCharacter()
+
+    aCharacter.health should be (1000)
+    aCharacter.level should be (1)
+  }
+
+
+}
