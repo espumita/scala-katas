f7ae3dcd5d26e5ef7f246941a39ca34b17964134
Espumita <davidfalconnaranjo@gmail.com>
2019-03-18 18:45:18 +0000

    Green: a character can health another character

--- src/main/scala/RPGCombat/GameCharacter.scala
+++ src/main/scala/RPGCombat/GameCharacter.scala
@@ -3,17 +3,26 @@ package RPGCombat
 case class GameCharacter() {
   var health: Int = 1000
 
-
   var level: Int = 1
+
+
   var isAlive: Boolean = true
+
 
   def dealDamageTo(focusCharacter: GameCharacter, damage: Int) : Unit = {
     focusCharacter.receivesDamage(damage)
   }
-
   def receivesDamage(damage: Int) : Unit ={
     health = health - damage
     if (health < 0) isAlive = false
+  }
+
+  def healthTo(focusCharacter: GameCharacter, healthPoints: Int) : Unit = {
+    focusCharacter.receivesHealth(healthPoints)
+  }
+
+  def receivesHealth(healthPoints: Int): Unit = {
+    health = health + healthPoints
   }
 
 
--- src/test/scala/RPGCombat/CharacterShould.scala
+++ src/test/scala/RPGCombat/CharacterShould.scala
@@ -33,5 +33,16 @@ class CharacterShould extends WordSpec with Matchers {
     anotherCharacter.isAlive should be (false)
   }
 
+  "a character can health another character" in {
+    val aCharacter = GameCharacter()
+    val anotherCharacter = GameCharacter()
+    anotherCharacter.receivesDamage(100)
+
+    aCharacter.healthTo(anotherCharacter, 100)
+
+    anotherCharacter.health should be (1000)
+  }
+
+
 
 }
